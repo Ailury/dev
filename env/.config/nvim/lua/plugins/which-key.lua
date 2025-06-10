555f0c418f343db1f239991d1566bf76e5419970
Ailury <aileris@proton.me>
2025-06-10 23:38:57 +0200

    Initial commit

--- env/.config/nvim/lua/plugins/which-key.lua
+++ env/.config/nvim/lua/plugins/which-key.lua
@@ -1,21 +1,5 @@
 -- I have a separate config.mappings file where I require which-key.
 -- With lazy the plugin will be automatically loaded when it is required somewhere
--- 
 return {
- "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
-}
+     "folke/which-key.nvim", lazy = true
+    }
