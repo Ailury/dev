d5d8fdee7a0ea02e0f62f79a896260da56eab912
Ailury <aileris@proton.me>
2025-06-11 00:25:31 +0200

    Initial commit

--- env/.config/nvim/lua/config/lazy.lua
+++ env/.config/nvim/lua/config/lazy.lua
@@ -21,6 +21,24 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Center the cursor on the screen when moving up or down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- same but with search
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste over highlight without losing clipboard
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- yank to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- search in selection in visual mode
+vim.keymap.set("v", "<leader>/", "<esc>/\\%V")
+vim.wo.relativenumber = true
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
--- env/.config/nvim/lua/plugins/lsp.lua
+++ env/.config/nvim/lua/plugins/lsp.lua
@@ -8,8 +8,8 @@ return {
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
+        -- "L3MON4D3/LuaSnip",
+        --"saadparwaiz1/cmp_luasnip",
         --"j-hui/fidget.nvim",
     },
 
@@ -26,7 +26,7 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
-                "lua_ls",
+                --"lua_ls",
                 "basedpyright",
                 "yamlls",
             },
@@ -37,44 +37,10 @@ return {
                         capabilities = capabilities
                     }
                 end,
-
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
             }
         })
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            })
-        })
 
         vim.diagnostic.config({
             -- update_in_insert = true,
