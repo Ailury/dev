126ee0c537509858e4af042658ab8a0beca3d123
Ailury <aileris@proton.me>
2025-06-25 23:53:05 +0200

    Initial commit

--- env/.config/nvim/lua/plugins/which-key.lua
+++
@@ -1,5 +0,0 @@
--- I have a separate config.mappings file where I require which-key.
--- With lazy the plugin will be automatically loaded when it is required somewhere
-return {
-     "folke/which-key.nvim", lazy = true
-    }
