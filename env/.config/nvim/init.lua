7e005bbed184ccf8e82ec40ab17028dad43aea23
Ailury <aileris@proton.me>
2025-08-17 23:54:45 +0200

    Initial commit

--- env/.config/nvim/init.lua
+++ env/.config/nvim/init.lua
@@ -1,16 +1,8 @@
-if vim.loader then
-  vim.loader.enable()
-end
+vim.g.mapleader = " "
 
-require("opts").initial()
-
-local lazy_path = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
-if not vim.uv.fs_stat(lazy_path) then
-  local lazy_url = "https://github.com/folke/lazy.nvim"
-  vim.fn.system { "git", "clone", "--filter=blob:none", lazy_url, "--branch=stable", lazy_path }
-end
-
-vim.opt.rtp:prepend(lazy_path)
-
-require "lazy_spec"
+require("base.globals")
+require("base.options")
+require("base.clipboard")
+require("base.autocmds")
+require("base.keymaps")
+require("base.lazy")
